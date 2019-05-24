a7bcab4aa6a3dc831dd98c604252c74bf45dd5bd
Robert Schneider <robmschn@gmail.com>
2019-05-24 11:25:01 -0400

    Update ResetSwitch.py

--- ResetSwitch.py
+++ ResetSwitch.py
@@ -9,7 +9,7 @@
 
 # Decription:
 # 	This script when ran with SecureCRT will reset a switch from ROMMON mode.
-#   The script will wait for outputs from the switch, so it can be ran
+#       The script will wait for outputs from the switch, so it can be ran
 #	continuously while you change from switch to switch.
 
 # Starts loop
